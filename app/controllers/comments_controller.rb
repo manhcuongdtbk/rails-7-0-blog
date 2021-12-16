9a5c1d3ed9610aba1dac9f8e86e6a4459a58b763
manhcuongdtbk <manhcuongdtbk@gmail.com>
2021-12-16 18:16:49 +0700

    Add CommentMailer to send submitted email after successfully creating a comment for a post

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,7 +2,8 @@ class CommentsController < ApplicationController
   before_action :set_post
 
   def create
-    @post.comments.create! params.required(:comment).permit(:content)
+    comment = @post.comments.create! params.required(:comment).permit(:content)
+    CommentsMailer.with(comment: comment).submitted.deliver_later
     redirect_to @post
   end
 
